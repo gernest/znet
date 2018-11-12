9678110aeea93a9ce999c2459b3154399124ed62
gernest <geofreyernest@live.com>
2018-11-12 13:01:57 +0300

    add isTokenTable fn

--- src/textproto/main.zig
+++ src/textproto/main.zig
@@ -7,4 +7,13 @@ pub const TextProto = struct.{
     pub const MIMEHeader = struct.{
         header: std.HashMap([]const u8, [][]const u8, mem.hash_slice_u8, mem.eql_slice_u8),
     };
+
+    allocator: *mem.Allocator,
 };
+
+fn isTokenTable(ch: u8) bool {
+    return switch (ch) {
+        '!', '#', '$', '%', '&', '\\', '*', '+', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'V', 'X', 'Y', 'Z', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '|', '~' => true,
+        else => false,
+    };
+}
